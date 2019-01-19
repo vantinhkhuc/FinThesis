348f396e162940d88b50af1faf41e9242cee7e9a
iMacUser <tungit@gmail.com>
2019-01-19 15:52:39 +0700

    commit 19-01

--- example.py
+++ example.py
@@ -1,10 +1,13 @@
 from openpyxl import load_workbook
 from pathlib import Path
 
-book_resource = load_workbook('excel/FIREANT_dscp_Nam.xlsx')
+book_resource = load_workbook('excel/FIREANT_dscp_Quy.xlsx')
+
 name = 'AAA'
-sheet  = book_resource[name]
-print(sheet.cell(1,1))
+sheet = book_resource[name]
+for row in sheet.iter_rows(min_row=1,min_col=1,max_row=sheet.max_row, max_col=sheet.max_column):
+    print(row[3].value)
+#print(sheet.cell(1,1))
 #type = 'Nam'
 #if type =='Nam':
 #    print('exist')
